6ccc195a61388dd6a819d50ee65e37acbd9c211e
unknown <>
2017-02-15 21:26:05 -0600

    adding my files to my repository

---
+++ Scripts/Main.js
@@ -0,0 +1,6 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML='Hello,World';
+//一点意思
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
